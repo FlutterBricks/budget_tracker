9349c07798da7450fd62440706210944d14d0f1e
Nicolas <nicolascamara29@gmail.com>
2022-03-28 11:26:55 -0700

    Nick: creating an item dynamically

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:percent_indicator/circular_percent_indicator.dart';
 
 import '../model/transaction_item.dart';
@@ -19,9 +20,17 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          setState(() {
-            items.add(TransactionItem(amount: 5.99, itemTitle: "Food"));
-          });
+          showDialog(
+              context: context,
+              builder: (context) {
+                return AddTransactionDialog(
+                  itemToAdd: (transactionItem) {
+                    setState(() {
+                      items.add(transactionItem);
+                    });
+                  },
+                );
+              });
         },
         child: const Icon(Icons.add),
       ),
@@ -81,6 +90,85 @@ class _HomePageState extends State<HomePage> {
   }
 }
 
+class AddTransactionDialog extends StatefulWidget {
+  final Function(TransactionItem) itemToAdd;
+  const AddTransactionDialog({required this.itemToAdd, Key? key})
+      : super(key: key);
+
+  @override
+  State<AddTransactionDialog> createState() => _AddTransactionDialogState();
+}
+
+class _AddTransactionDialogState extends State<AddTransactionDialog> {
+  final TextEditingController itemTitleController = TextEditingController();
+  final TextEditingController amountController = TextEditingController();
+
+  bool _isExpenseController = true;
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width / 1.3,
+        height: 300,
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Column(
+            children: [
+              const Text(
+                "Add an expense",
+                style: TextStyle(fontSize: 18),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              TextField(
+                controller: itemTitleController,
+                decoration: const InputDecoration(hintText: "Name of expense"),
+              ),
+              TextField(
+                controller: amountController,
+                keyboardType: TextInputType.number,
+                inputFormatters: <TextInputFormatter>[
+                  FilteringTextInputFormatter.digitsOnly
+                ],
+                decoration: const InputDecoration(hintText: "Amount in \$"),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text("Is expense?"),
+                  Switch.adaptive(
+                      value: _isExpenseController,
+                      onChanged: (b) {
+                        setState(() {
+                          _isExpenseController = b;
+                        });
+                      })
+                ],
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    if (amountController.text.isNotEmpty &&
+                        itemTitleController.text.isNotEmpty) {
+                      widget.itemToAdd(TransactionItem(
+                          amount: double.parse(amountController.text),
+                          itemTitle: itemTitleController.text,
+                          isExpense: _isExpenseController));
+                      Navigator.pop(context);
+                    }
+                  },
+                  child: const Text("Add"))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
 class TransactionCard extends StatelessWidget {
   final TransactionItem item;
   const TransactionCard({required this.item, Key? key}) : super(key: key);
@@ -112,7 +200,7 @@ class TransactionCard extends StatelessWidget {
             ),
             const Spacer(),
             Text(
-              (item.isExpense ? "+ " : "- ") + item.amount.toString(),
+              (!item.isExpense ? "+ " : "- ") + "\$" + item.amount.toString(),
               style: const TextStyle(
                 fontSize: 16,
               ),
